8595aef96eef8da720e48984c5dd4e4accecc26b
lombold <colin.disler@gmail.com>
2025-04-14 21:08:11 +0200

    feat(zone-example) adds deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {routes} from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     // provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/angular-change-detection-demo',
+    },
+  ],
 };
